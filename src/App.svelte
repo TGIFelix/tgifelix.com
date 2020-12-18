071b339c6c26f11bb7a19ddcbcdd4c306c899218
Felix Swinkels <tgifelix@icloud.com>
2020-12-18 02:20:45 +0100

    meta

--- src/App.svelte
+++ src/App.svelte
@@ -17,8 +17,8 @@
 
 <svelte:head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <meta name="description" content="" />
-  <meta name="keywords" content="" />
+  <meta name="description" content="Junior Front End Developer" />
+  <meta name="keywords" content="felix, swinkels, webdev, frontend, developer" />
   <meta name="author" content="Felix Swinkels, @_tgifelix" />
   <title>I'm for hire!</title>
 </svelte:head>
